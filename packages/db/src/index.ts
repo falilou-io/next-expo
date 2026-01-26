809e2c688e6fdb9fe6622e8470ff08db83982bad
Falilou <mouhamadou@falilou.io>
2026-01-26 17:32:29 -0500

    initial commit

---
+++ packages/db/src/index.ts
@@ -0,0 +1,6 @@
+import { env } from "@mono-nooto/env/server";
+import { drizzle } from "drizzle-orm/node-postgres";
+
+import * as schema from "./schema";
+
+export const db = drizzle(env.DATABASE_URL, { schema });
